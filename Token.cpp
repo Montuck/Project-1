5da5cae9a54125718930274fde6cce5985afaea2
Trevor <trevor_wiseman@outlook.com>
2021-09-16 15:29:03 -0600

    Add initial files

--- Token.cpp
+++ Token.cpp
@@ -11,58 +11,58 @@ string Token::toString() {
     string output = "";
     switch(type) {
         case TokenType::COLON:
-            output = "(COLON, \":\", " + to_string(lineNumber) + ")";
+            output = "(COLON,\":\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::COLON_DASH:
-            output = "(COLON_DASH, \":-\", " + to_string(lineNumber) + ")";
+            output = "(COLON_DASH,\":-\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::COMMA:
-            output = "(COMMA, \",\", " + to_string(lineNumber) + ")";
+            output = "(COMMA,\",\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::PERIOD:
-            output = "(PERIOD, \".\", " + to_string(lineNumber) + ")";
+            output = "(PERIOD,\".\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::Q_MARK:
-            output = "(Q_MARK, \"?\", " + to_string(lineNumber) + ")";
+            output = "(Q_MARK,\"?\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::LEFT_PAREN:
-            output = "(LEFT_PAREN, \"(\", " + to_string(lineNumber) + ")";
+            output = "(LEFT_PAREN,\"(\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::RIGHT_PAREN:
-            output = "(RIGHT_PAREN, \")\", " + to_string(lineNumber) + ")";
+            output = "(RIGHT_PAREN,\")\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::MULTIPLY:
-            output = "(MULTIPLY, \"*\", " + to_string(lineNumber) + ")";
+            output = "(MULTIPLY,\"*\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::ADD:
-            output =  "(ADD, \"+\", " + to_string(lineNumber) + ")";
+            output =  "(ADD,\"+\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::SCHEMES:
-            output = "(SCHEMES, \"Schemes\", " + to_string(lineNumber) + ")";
+            output = "(SCHEMES,\"Schemes\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::FACTS:
-            output = "(FACTS, \"Facts\", " + to_string(lineNumber) + ")";
+            output = "(FACTS,\"Facts\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::RULES:
-            output = "(RULES, \"Rules\", " + to_string(lineNumber) + ")";
+            output = "(RULES,\"Rules\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::QUERIES:
-            output = "(QUERIES, \"Queries\", " + to_string(lineNumber) + ")";
+            output = "(QUERIES,\"Queries\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::ID:
-            output = "(ID, \"" + value + "\", " + to_string(lineNumber) + ")";
+            output = "(ID,\"" + value + "\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::STRING:
-            output = "(STRING, \"" + value + "\", " + to_string(lineNumber) + ")";
+            output = "(STRING,\"" + value + "\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::COMMENT:
-            output = "(COMMENT, \"" + value + "\", " + to_string(lineNumber) + ")";
+            output = "(COMMENT,\"" + value + "\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::EFO:
-            output = "(EOF, \"\", " + to_string(lineNumber) + ")";
+            output = "(EOF,\"\"," + to_string(lineNumber) + ")";
             break;
         case TokenType::UNDEFINED:
-            output = "(UNDEFINED, \"$\", " + to_string(lineNumber) + ")";
+            output = "(UNDEFINED,\"$\"," + to_string(lineNumber) + ")";
             break;
         default: return "nothing"; break;
     }
